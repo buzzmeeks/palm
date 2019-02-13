8129cde4ebf448778ce7f5f9fad1d3200f6414a4
Aurélie VIOLETTE <orwel.violette@gmail.com>
2019-02-13 20:23:33 +0100

    Ajustements formattage Aurélie

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,14 +1,16 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-        <p>
-        <strong>Mail</strong>: parisleaguemodern@gmail.com
-        </p>
-        <p>
-           <a href=https://www.facebook.com/Dessine-moi-un-Gobelin-843181532683583>Facebook</a>
-            </p>
-        </v-flex>
-        
-
+      <ul>
+        <li>
+          <strong>Mail</strong>:
+          <a href="mailto:parisleaguemodern@gmail.com">parisleaguemodern@gmail.com</a>
+        </li>
+        <li>
+          <strong>Facebook</strong>
+          <a href="https://www.facebook.com/Dessine-moi-un-Gobelin-843181532683583">Facebook</a>
+        </li>
+      </ul>
+    </v-flex>
   </v-layout>
 </template>
