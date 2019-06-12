03a7c0ea8c82cf8aa6bca79320ac50f4f153bb35
Adrien Baumann <adrien.baumann@gmail.com>
2019-06-12 20:28:39 +0200

    Adds more info on tournament endpoint

--- server/db/models/tournaments/tournaments.model.js
+++ server/db/models/tournaments/tournaments.model.js
@@ -4,13 +4,34 @@ const q = require('../../client')
 
 function getAll({ lid }) {
   return q('tournaments')
-    .select('id', 'shop', 'type', 'date')
+    .select('id', 'shop', 'type', 'date', 'matches')
     .then(t =>
       t.sort((a, b) => {
         if (a.date < b.date) {
           return 1
         }
         return -1
+      }),
+    )
+    .then(t =>
+      t.map(thatTournament => {
+        let swiss = 0
+        let top8 = false
+        const players = []
+        thatTournament.matches.forEach(r => {
+          r.forEach(m => {
+            const nbRound = parseInt(m.round)
+            if (nbRound > swiss && m.top8 === false) {
+              swiss = nbRound
+            }
+            top8 = m.top8
+            players.push(m.dci)
+          })
+        })
+        thatTournament.nbSwiss = swiss
+        thatTournament.top8 = top8
+        thatTournament.players = _.uniq(players).length
+        return thatTournament
       }),
     )
     .then(t => _.groupBy(t, 'shop'))
